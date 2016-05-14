cbf9e0e854d15b2d839e48201a04d555b4368671
Sabdi Abraham Pantoja Orozco <sabdi_20@outlook.com>
2016-05-13 22:31:50 -0500

    Clase Reloj

--- RG/src/rg/Reloj.java
+++ RG/src/rg/Reloj.java
@@ -1,10 +1,24 @@
 
 package rg;
-public class Reloj {
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Reloj implements Runnable{
     int cuenta, valor;
     
     public Reloj(int v) {
         cuenta = 0;
     }
     
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(1000);
+                cuenta++;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Reloj.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
